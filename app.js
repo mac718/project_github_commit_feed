93e95ffbe2555d78d75552787de16b78c9d75c2f
mac718 <mac718@gmail.com>
2018-10-06 18:27:29 -0400

    Enable dynamic rendering of JSON file on index page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const wrapper = require('./lib/wrapper');
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const commitFeed = require('./data/commits.json');
 
 const server = http.createServer((req, res) => {
   fs.readFile('./public/index.html', (err, data) => {
@@ -12,11 +13,13 @@ const server = http.createServer((req, res) => {
     } else {  
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html');
-      res.end(data);
+      let dummyFeed = JSON.stringify(commitFeed, null, 2);
+      let body = data.toString().replace("{{commitFeed}}", dummyFeed);
+      res.end(body);
     }
   })
 })
 
 server.listen(3000, 'localhost', () => {
-  console.log('listening bitch');
+  console.log('listening');
 });
